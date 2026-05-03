8490e3129da5ae0d54dc52208a4d01969481b7c3
Riccardo Alfonso Cerrone <riccardo.alfonsocer@outlook.it>
2026-05-03 10:56:39 +0200

    Attention layer implemented

--- modules/attention.py
+++ modules/attention.py
@@ -32,9 +32,27 @@ class CausalSelfAttention(nn.Module):
     return proj
 
   def attention(self, key, query, value, attention_mask):
-
     ### YOUR CODE HERE
-    raise NotImplementedError
+    
+    # Recall that key, query and value have the following dimensions
+    # Batch size,  Number of heads, Sequence length, Attention head size/dimension
+    # Calculates the first scores: Q @ K.T
+    scores = torch.matmul(query, key.transpose(-1, -2))
+    # Retrives the attention dimension from the query (same would have been for key or value)
+    attention_dim = query.shape[-1]
+    # Normalization by the attention dimension
+    scores = scores / (attention_dim ** 0.5)
+    # Application of the attention mask
+    scores = scores.masked_fill(attention_mask == 0, float("-inf"))
+    # Apply softmax
+    attn_probs = scores.softmax(dim=-1)
+    # Apply dropout - optional (as suggested in the __init__)
+    attn_probs = self.dropout(attn_probs)
+    # Last multiplication
+    values = torch.matmul(attn_probs, value)
+    # Concat
+    attn_values = rearrange(values, "b h t d -> b t (h d)")
+    return attn_values
 
 
   def forward(self, hidden_states, attention_mask):
